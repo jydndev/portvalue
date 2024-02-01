3a18956b5c3193670c8ec21df57cd3d855ad7c34
김지욱 <nhncommerce@NHNcommerceui-MacBookPro.local>
2024-02-01 15:02:44 +0900

    :bookmark: v1.3.15 릴리즈

---
+++ src/constants/i18n/kr/index.js
@@ -0,0 +1,25 @@
+import auth from './auth.json';
+import claim from './claim.json';
+import common from './common.json';
+import form from './form.json';
+import main from './main.json';
+import manage from './manage.json';
+import order from './order.json';
+import provider from './provider.json';
+import title from './title.json';
+import unit from './unit.json';
+
+const resourceKr = {
+  common,
+  main,
+  auth,
+  provider,
+  title,
+  order,
+  manage,
+  unit,
+  form,
+  claim,
+};
+
+export default resourceKr;
