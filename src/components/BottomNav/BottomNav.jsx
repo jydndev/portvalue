dd6d859e9c25b1655515def8279c6561ccace908
portvalue1 <portvalue@shopby.works>
2024-06-19 15:19:25 +0900

    [메인] 배너 수정

--- src/components/BottomNav/BottomNav.jsx
+++ src/components/BottomNav/BottomNav.jsx
@@ -2,22 +2,38 @@ import { Link } from 'react-router-dom';
 
 import { string, func } from 'prop-types';
 
-import { IconSVG } from '@shopby/react-components';
+import { HomeIcon } from '../Icon/HomeIcon';
+import { SearchIcon } from '../Icon/SearchIcon';
+import { MypageIcon } from '../Icon/MypageIcon';
+import { HamburgerIconBottom } from '../Icon/HamburgerIconBottom';
+import { useOffCanvasActionContext } from '@shopby/react-components';
 
-const BottomNav = ({ className = '', search }) => (
-  <nav className={`bottom-nav ${className}`}>
-    <Link to="/" className="bottom-nav__link bottom-nav__home">
-      <IconSVG size={55} name="home" strokeWidth={4} />
-      <span className="bottom-nav__label">홈</span>
-    </Link>
-    <button className="bottom-nav__link bottom-nav__link--search" onClick={search}>
-      <span className="bottom-nav__label">검색</span>
-    </button>
-    <Link to="my-page" className="bottom-nav__link bottom-nav__link--north">
-      <span className="bottom-nav__label">마이페이지</span>
-    </Link>
-  </nav>
-);
+const BottomNav = ({ className = '', search }) => {
+  const { openCanvas } = useOffCanvasActionContext();
+
+  return (
+    <>
+      <nav className={`bottom-nav ${className}`}>
+        <Link to="/" className="bottom-nav__link-btn">
+          <HomeIcon size={28} />
+          <span className="bottom-nav__label">홈</span>
+        </Link>
+        <button className="bottom-nav__link-btn" onClick={openCanvas}>
+          <HamburgerIconBottom size={28} />
+          <span className="bottom-nav__label">카테고리</span>
+        </button>
+        <button className="bottom-nav__link-btn" onClick={search}>
+          <SearchIcon size={28} />
+          <span className="bottom-nav__label">검색</span>
+        </button>
+        <Link to="my-page" className="bottom-nav__link-btn">
+          <MypageIcon size={28} />
+          <span className="bottom-nav__label">마이페이지</span>
+        </Link>
+      </nav>
+    </>
+  );
+};
 
 BottomNav.propTypes = {
   className: string,
