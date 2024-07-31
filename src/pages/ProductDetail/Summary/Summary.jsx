73c6fe0354babe408d20fad089b920cc2f3adfe6
portvalue1 <portvalue@shopby.works>
2024-07-31 13:32:40 +0900

    [상세페이지] 신규 회원가입 버튼 추가

--- src/pages/ProductDetail/Summary/Summary.jsx
+++ src/pages/ProductDetail/Summary/Summary.jsx
@@ -1,14 +1,42 @@
-import { useProductDetailStateContext } from '@shopby/react-components';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { useProductDetailStateContext, useAuthStateContext, useAuthActionContext } from '@shopby/react-components';
+import { isSignedIn } from '@shopby/shared';
 
 import AccumulationInformation from './AccumulationInformation';
 import DownloadCouponButton from './DownloadCouponButton';
 import FreightInformation from './FreightInformation';
 import PriceInformation from './PriceInformation';
+import { RightIcon } from '../../../components/Icon/RightIcon';
+
 
 const Summary = () => {
   const {
     productDetail: { summary },
   } = useProductDetailStateContext();
+  const navigate = useNavigate();
+  const { profile } = useAuthStateContext();
+  const { fetchProfile } = useAuthActionContext();
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const checkLoginStatus = async () => {
+      if (isSignedIn()) {
+        if (!profile) {
+          await fetchProfile();
+        }
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+    };
+
+    checkLoginStatus();
+  }, [isSignedIn(), profile]);
+
+  const handleNewUserDiscountClick = () => {
+    navigate('/sign-in');
+  };
 
   return (
     <div className="product-summary">
@@ -20,11 +48,24 @@ const Summary = () => {
         <PriceInformation {...summary} />
         <DownloadCouponButton />
       </div>
+      <div className="new-user-discount">
+        {!isLoggedIn && (
+          <button className="new-user-discount-button" onClick={handleNewUserDiscountClick}>
+            <div className="button-text">
+              <span className="black-text">신규가입 시 </span>
+              <span className="blue-text">최대 3,000원 </span>
+              <span className="black-text">할인</span>
+            </div>
+            <RightIcon className="right-icon" />
+          </button>
+        )}
+      </div>
       <div className="product-summary__freight-line">
-        <FreightInformation {...summary.freight} />
         <AccumulationInformation {...summary.accumulation} />
+        <FreightInformation {...summary.freight} />
       </div>
     </div>
   );
 };
+
 export default Summary;
