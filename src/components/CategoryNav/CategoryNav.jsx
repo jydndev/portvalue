fa2b7d1b3192f769c8ca4fcae560c90b923ef014
jydnbach <jaydenkim95@gmail.com>
2024-08-01 18:42:00 +0900

    omit layoutchanger in category nav

--- src/components/CategoryNav/CategoryNav.jsx
+++ src/components/CategoryNav/CategoryNav.jsx
@@ -2,12 +2,6 @@ import useLayoutChanger from '../../hooks/useLayoutChanger';
 import CategoryNavLinks from './CategoryNavLinks';
 
 const CategoryNav = () => {
-  useLayoutChanger({
-    hasBackBtnOnHeader: true,
-    hasBottomNav: true,
-    title: '카테고리',
-  });
-
   return (
     <div className="category-nav-page">
       <CategoryNavLinks />
