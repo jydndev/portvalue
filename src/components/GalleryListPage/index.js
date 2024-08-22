41d5f8a55e215cb4cb72d996fb3fa3bef7aa449b
jydnbach <jaydenkim95@gmail.com>
2024-08-22 18:12:54 +0700

    exports in index.js

--- src/components/GalleryListPage/index.js
+++ src/components/GalleryListPage/index.js
@@ -1,3 +1,9 @@
 import GalleryListPage from './GalleryListPage';
+import ProductList from './ProductList';
+import ProductItem from './ProductItem';
+import NoSearchProduct from './NoSearchProduct';
+import SkeletonComponent from './SkeletonComponent';
+
+export { GalleryListPage, ProductList, ProductItem, NoSearchProduct, SkeletonComponent };
 
 export default GalleryListPage;
