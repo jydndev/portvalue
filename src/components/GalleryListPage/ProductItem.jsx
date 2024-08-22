8067ed32a04d91087f6712b0cd8707c69b016200
jydnbach <jaydenkim95@gmail.com>
2024-08-22 20:17:53 +0700

    ProductItem.jsx: fix return statement and put calculation logic inside react component

--- src/components/GalleryListPage/ProductItem.jsx
+++ src/components/GalleryListPage/ProductItem.jsx
@@ -4,11 +4,6 @@ import { calculateDiscountedPrice } from '@shopby/shared';
 import ProductThumbBadge from '../ProductThumbBadge';
 import ProductThumbInfo from '../ProductThumbInfo';
 
-const calculateDiscountRate = (originalPrice, discountedPrice) => {
-  if (originalPrice === discountedPrice) return 0;
-  return Math.round(((originalPrice - discountedPrice) / originalPrice) * 100);
-};
-
 const ProductItem = ({
   productNo,
   adult,
@@ -21,38 +16,45 @@ const ProductItem = ({
   immediateDiscountAmt,
   additionDiscountAmt,
   brandName,
-}) => (
-  <ThumbItem
-    key={productNo}
-    resize="220x220"
-    href={`/product-detail?productNo=${productNo}`}
-    src={listImageUrls[0]}
-    adult={adult}
-    alt={productName}
-    productNo={productNo}
-  >
-    <ProductThumbBadge isSoldOut={isSoldOut} saleStatusType={saleStatusType} />
-    <Link to={`/product-detail?productNo=${productNo}`}>
-      <ProductThumbInfo
-        salePrice={calculateDiscountedPrice({
-          salePrice,
-          immediateDiscountAmt,
-          additionDiscountAmt,
-        })}
-        brandName={brandName}
-        promotionText={promotionText}
-        productName={productName}
-        discountRate={calculateDiscountRate(
-          salePrice,
-          calculateDiscountedPrice({
+}) => {
+  const calculateDiscountRate = (originalPrice, discountedPrice) => {
+    if (originalPrice === discountedPrice) return 0;
+    return Math.round(((originalPrice - discountedPrice) / originalPrice) * 100);
+  };
+
+  return (
+    <ThumbItem
+      key={productNo}
+      resize="220x220"
+      href={`/product-detail?productNo=${productNo}`}
+      src={listImageUrls[0]}
+      adult={adult}
+      alt={productName}
+      productNo={productNo}
+    >
+      <ProductThumbBadge isSoldOut={isSoldOut} saleStatusType={saleStatusType} />
+      <Link to={`/product-detail?productNo=${productNo}`}>
+        <ProductThumbInfo
+          salePrice={calculateDiscountedPrice({
             salePrice,
             immediateDiscountAmt,
             additionDiscountAmt,
-          })
-        )}
-      />
-    </Link>
-  </ThumbItem>
-);
+          })}
+          brandName={brandName}
+          promotionText={promotionText}
+          productName={productName}
+          discountRate={calculateDiscountRate(
+            salePrice,
+            calculateDiscountedPrice({
+              salePrice,
+              immediateDiscountAmt,
+              additionDiscountAmt,
+            })
+          )}
+        />
+      </Link>
+    </ThumbItem>
+  );
+};
 
 export default ProductItem;
