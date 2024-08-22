3a6b5eea09938fd9d6ca5913f7fa7011c47b6b4b
jydnbach <jaydenkim95@gmail.com>
2024-08-22 17:59:12 +0700

    lean GalleryListPage.jsx

--- src/components/GalleryListPage/GalleryListPage.jsx
+++ src/components/GalleryListPage/GalleryListPage.jsx
@@ -1,24 +1,9 @@
-import { useMemo } from 'react';
-import { Link, useSearchParams } from 'react-router-dom';
-
 import { object, bool, func, array, number, string } from 'prop-types';
-
-import { Icon, InfiniteScrollLoader, ThumbItem, ThumbList, VisibleComponent } from '@shopby/react-components';
-import { calculateDiscountedPrice, THUMB_LIST_TYPE } from '@shopby/shared';
-
-import GallerySkeleton from '../GallerySkeleton';
-import ProductThumbBadge from '../ProductThumbBadge';
-import ProductThumbInfo from '../ProductThumbInfo';
+import { InfiniteScrollLoader, VisibleComponent } from '@shopby/react-components';
 import TotalCountAndSort from '../TotalCountAndSort';
-
+import ProductList from './ProductList';
 import NoSearchProduct from './NoSearchProduct';
-
-const calculateDiscountRate = (originalPrice, discountedPrice) => {
-  if (originalPrice === discountedPrice) return 0;
-  return Math.round(((originalPrice - discountedPrice) / originalPrice) * 100);
-};
-
-const SkeletonComponent = ({ isLoading }) => <GallerySkeleton rowCount={3} colCount={2} isLoading={isLoading} />;
+import SkeletonComponent from './SkeletonComponent';
 
 const GalleryListPage = ({
   style,
@@ -39,58 +24,7 @@ const GalleryListPage = ({
       shows={products.length > 0}
       TruthyComponent={
         <>
-          <ThumbList style={style} displayType={THUMB_LIST_TYPE.GALLERY} className={className}>
-            {products.map(
-              ({
-                productNo,
-                adult,
-                listImageUrls,
-                isSoldOut,
-                saleStatusType,
-                salePrice,
-                promotionText,
-                productName,
-                immediateDiscountAmt,
-                additionDiscountAmt,
-                frontDisplayYn,
-                brandName,
-              }) =>
-                frontDisplayYn && (
-                  <ThumbItem
-                    key={productNo}
-                    resize="220x220"
-                    href={`/product-detail?productNo=${productNo}`}
-                    src={listImageUrls[0]}
-                    adult={adult}
-                    alt={productName}
-                    productNo={productNo}
-                  >
-                    <ProductThumbBadge isSoldOut={isSoldOut} saleStatusType={saleStatusType} />
-
-                    <Link to={`/product-detail?productNo=${productNo}`}>
-                      <ProductThumbInfo
-                        salePrice={calculateDiscountedPrice({
-                          salePrice,
-                          immediateDiscountAmt,
-                          additionDiscountAmt,
-                        })}
-                        brandName={brandName}
-                        promotionText={promotionText}
-                        productName={productName}
-                        discountRate={calculateDiscountRate(
-                          salePrice,
-                          calculateDiscountedPrice({
-                            salePrice,
-                            immediateDiscountAmt,
-                            additionDiscountAmt,
-                          })
-                        )}
-                      />
-                    </Link>
-                  </ThumbItem>
-                )
-            )}
-          </ThumbList>
+          <ProductList products={products} style={style} className={className} />
           <SkeletonComponent isLoading={isLoading} />
           <InfiniteScrollLoader onIntersect={handleIntersect} disabled={disabled} />
         </>
@@ -100,11 +34,6 @@ const GalleryListPage = ({
   </div>
 );
 
-export default GalleryListPage;
-
-SkeletonComponent.propTypes = {
-  isLoading: bool,
-};
 GalleryListPage.propTypes = {
   style: object,
   totalCount: number,
